5cc44b1e371b08d5ff086c171188c2ed7de82090
DmitriiK <dklmn@mail.ru>
2025-06-07 17:31:58 +0300

    cleaning up of DB

--- init.py
+++ init.py
@@ -13,7 +13,12 @@ def init_db():
         )''')
     print(f"Database {config.DB_PATH} initialized with table 'pdfs'.")
 
+def delete_all():
+    with sqlite3.connect(config.DB_PATH) as conn:
+        conn.execute('''DELETE FROM pdfs''')
+    print("All records deleted from the 'pdfs' table.")
 
 if __name__ == "__main__":
     init_db()
+    # delete_all()
 
